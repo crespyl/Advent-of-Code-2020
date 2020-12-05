d8ec225f520a8aa20034c01e5a36d848c1206295
Peter Jacobs <peter@crespyl.net>
2020-12-04 23:35:38 -0600

    day 5

---
+++ day5/day5.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+def bstep(path, low, high)
+  steps = path.chars
+  while ! steps.empty?
+    c = steps.shift
+    case c
+    when /F|L/
+      high -= (high - low)/2
+    when /B|R/
+      low += (high - low)/2
+    end
+  end
+  return low
+end
+
+def find_seat_score(pass)
+  row = bstep(pass[0...7], 0, 128)
+  col = bstep(pass[7...], 0, 8)
+  return (row * 8) + col
+end
+
+input = File.read(ARGV[0] || "test.txt")
+
+puts "Part 1"
+puts input.lines.map { |line| find_seat_score(line) }.max
+
+puts "Part 2"
+seats = input.lines.map { |line| find_seat_score(line) }.sort
+seats[1..].reduce(seats.first) do |prev, cur|
+  if prev+1 != cur
+    puts prev+1
+    break
+  else
+    cur
+  end
+end
