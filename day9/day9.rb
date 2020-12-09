a411405f16c25428b864ea6c235f46945446700b
Peter Jacobs <peter@crespyl.net>
2020-12-09 02:26:10 -0600

    build in unit test and add template

--- day9/day9.rb
+++ day9/day9.rb
@@ -1,31 +1,45 @@
 #!/usr/bin/env ruby
-require('pry')
 
-PREAMBLE_LENGTH = 25
+require 'benchmark'
+require 'minitest'
+require 'pry'
+
+TEST_STR = """\
+35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576
+"""
+
+class Test < MiniTest::Test
+  def test_p1
+    assert_equal(127, compute_p1(TEST_STR, 5))
+  end
+
+  def test_p2
+    assert_equal(62, compute_p2(TEST_STR, compute_p1(TEST_STR, 5)))
+  end
+end
 
 def valid?(preamble, number)
   preamble.combination(2).map { |a,b| a + b }.any?(number)
 end
-
-puts "Part 1"
-
-input = File.read(ARGV[0] || "test.txt").lines.map(&:to_i)
-buffer = input[0...PREAMBLE_LENGTH]
-target = nil
-
-input[PREAMBLE_LENGTH...].each do |n|
-  if ! valid?(buffer, n)
-    target = n
-    break;
-  else
-    buffer.shift
-    buffer << n
-  end
-end
-
-puts target
-
-puts "Part 2"
 
 def check_contiguous(list, target)
   sum = list[0]
@@ -36,11 +50,45 @@ def check_contiguous(list, target)
   end
 end
 
-input.each_with_index do |n, i|
-  match, len = check_contiguous(input[i...], target)
-  if match
-    span = input[i..i+len]
-    puts span.min() + span.max()
-    break
+def compute_p1(input, preamble_length)
+  input = input.lines.map(&:to_i)
+  buffer = input[0...preamble_length]
+
+  input[preamble_length...].each do |n|
+    if ! valid?(buffer, n)
+      return n
+    else
+      buffer.shift
+      buffer << n
+    end
   end
+end
+
+def compute_p2(input, target)
+  input = input.lines.map(&:to_i)
+  input.each_with_index do |n, i|
+    match, len = check_contiguous(input[i...], target)
+    if match
+      span = input[i..i+len]
+      return span.min() + span.max()
+    end
+  end
+end
+
+if MiniTest.run
+  puts "Test case OK, running..."
+
+  @input = File.read(ARGV[0] || "input.txt")
+
+  Benchmark.bm do |bm|
+    bm.report("Part 1:") { @p1 = compute_p1(@input, 25) }
+    bm.report("Part 2:") { @p2 = compute_p2(@input, @p1) }
+  end
+
+  puts "\nResults:"
+  puts "Part 1: %i" % @p1
+  puts "Part 2: %i" % @p2
+
+else
+  puts "Test case ERR"
 end
