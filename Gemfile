daf628dfe05e92429e00eb0f380bc21326b81082
Peter Jacobs <peter@crespyl.net>
2020-12-10 02:25:32 -0600

    Update getinput.rb, add Gemfile

---
+++ Gemfile
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+
+gem "benchmark"
+gem "minitest"
+gem "pry"
--- util/getinput.rb
+++ util/getinput.rb
@@ -7,9 +7,22 @@ require 'httparty'
 
 year = ARGV[0] || Time.now.year
 day = ARGV[1] || Time.now.day
-
 cookie = File.read(".env")
 url = "https://adventofcode.com/#{year}/day/#{day}/input"
-response = HTTParty.get(url, headers: { "Cookie" => cookie })
 
-puts response.body
+loop do
+  response = HTTParty.get(url, headers: { "Cookie" => cookie })
+  case response.code
+  when 404
+    STDERR.puts "not ready yet..."
+    sleep 1
+  when 200
+    puts response.body
+    break
+  else
+    puts "something happened:"
+    puts response.code
+    puts response.body
+    break
+  end
+end
